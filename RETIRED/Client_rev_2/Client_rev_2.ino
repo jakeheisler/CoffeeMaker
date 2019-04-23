dd646639369be507ed485786e2e22936c91d66dc
jakeheisler <35744439+jakeheisler@users.noreply.github.com>
2019-04-22 22:09:54 -0400

    modified controls
    AP is now constantly powered on. and client can disconnect any time.

--- RETIRED/Client_rev_2/Client_rev_2.ino
+++ RETIRED/Client_rev_2/Client_rev_2.ino
@@ -1,6 +1,7 @@
 
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <arduino.h>
 
 
 WiFiUDP udp;
@@ -12,12 +13,15 @@ const char *ssid = "CoffeeMaker8266";
 const char *pass = "CoffeePassword";
 
 
-
-char packetBuffer[255]; //buffer to hold incoming packet
-char  ReplyBuffer[] = "Client";
-
 int LED_BUILTINR = 5;
 unsigned int localPort = 2000; // local port to listen for UDP packets
+
+char packetBuffer[255]; //buffer to hold incoming packet
+char  ReplyBuffer[] = "L";
+
+int sw1 = 0;
+int BS = LOW;
+boolean toggle = false;
 
 //======================================================================
 //                Setup
@@ -58,31 +62,43 @@ void loop()
 
   int packetSize = udp.parsePacket();
   if (packetSize) {
-    //Serial.print("Received packet of size ");
-    //Serial.println(packetSize);
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
     //If serial data is recived send it to UDP
     int len = udp.read(packetBuffer, 255);
     if (len > 0) {
       packetBuffer[len] = 0;
     }
-    delay(20);
+    delay(10);
     Serial.println("Contents:");
     Serial.println(packetBuffer);
-
-    if (packetBuffer[0] == 'L') {
-      delay(20);
-      digitalWrite(LED_BUILTIN, LOW);
-      digitalWrite(LED_BUILTINR, LOW);
-    } else {
-      delay(20);
-      digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(LED_BUILTINR, HIGH);
-    }
-
-    delay(20);
   }
-  udp.beginPacket(ServerIP, 2000);
+  delay(10);
+  BS = digitalRead(sw1);
+  delay(10);
+  if (BS == LOW) {
+    while (BS == LOW) {
+      BS = digitalRead(sw1);
+      if (BS == HIGH) {
+        break;
+      }
+      delay(10);
+    }
+    toggle = !toggle;
+    if (toggle == true) {
+      strcpy(ReplyBuffer, "\0");
+      strcpy(ReplyBuffer, "L");
+      delay(10);
+    } else {
+      strcpy(ReplyBuffer, "\0");
+      strcpy(ReplyBuffer, "H");
+      delay(10);
+    }
+    delay(10);
+  }
+  //Serial.println(ReplyBuffer);
+  //data sent to client
+  udp.beginPacket(ClientIP, 2000);
   udp.write(ReplyBuffer);
   udp.endPacket();
-
 }
